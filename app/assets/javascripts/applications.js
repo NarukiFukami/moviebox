55c2687463f5c5d356b43a9c7b30855907dec4b9
Naruki <ovoxoomo@icloud.com>
2020-12-20 22:51:54 -0800

    created a new project and design template

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -1,2 +1,8 @@
-
+//= jquery/jquery.min
+//= bootstrap/js/bootstrap.bundle.min
+//= jquery.easing/jquery.easing.min
+//= php-email-form/validate
+//= aos/aos
+//= isotope-layout/isotope.pkgd.min
+//= owl.carousel/owl.carousel.min
 //= require main
